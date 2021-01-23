d5aad238dc5c2ec60095415496d1c560e9670f18
Tomas Oniščiukas <onisciukas.tomas@gmail.com>
2021-01-23 18:01:01 +0200

    Disabled Tailwind purge for showcasing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./**/*.html"],
+  // purge: ["./**/*.html"], // disabled purge due to dynamic content
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
