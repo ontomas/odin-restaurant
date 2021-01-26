2cc69604c0160cbae880eea39215276c44c0ed24
Tomas Oniščiukas <onisciukas.tomas@gmail.com>
2021-01-26 08:17:57 +0200

    Add CSS minify and purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // purge: ["./**/*.html"], // disabled purge due to dynamic content
+  purge: ["./**/*.html", "./**/*.js"], // disabled purge due to dynamic content
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
